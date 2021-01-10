7ba2ddc88972cd618933d6fd93b293baedfb15de
pangpan <pangpan@asiainfo.com>
2021-01-10 11:40:39 +0800

    支付服务注册进consul

---
+++ cloud-providerconsul-payment-8006/src/main/java/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author pangpan
+ * @date 2021-01-10
+ */
+@RestController
+@Slf4j
+@RequestMapping(value = "/payment")
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/consul")
+    public String paymentConsul()
+    {
+        return "springcloud with consul: "+serverPort+"\t   "+ UUID.randomUUID().toString();
+    }
+}
